41b02bf73355c058b504b2db3064eb69b1bb4a6e
emilys314 <emilys314@protonmail.com>
2022-06-25 22:48:47 -0400

    Cleaned up new implementation

--- src/entity_management/collision_area.h
+++ src/entity_management/collision_area.h
@@ -2,10 +2,8 @@
 #define COLLISION_AREA_H
 
 class CollisionArea {
-
-    bool isOverlapping1D(float xmin1, float xmax1, float xmin2, float xmax2) {
-        return (xmax1 >= xmin2) && (xmax2 >= xmin1);
-    }
+    float x_min, x_max;
+    float y_min, y_max;
 
     bool isMovingTowards(glm::vec2 test_point, glm::vec2 object_position, glm::vec2 object_velocity) {
         glm::vec2 to_point = test_point - object_position; //a vector going from your obect to the point
@@ -13,10 +11,6 @@ class CollisionArea {
     }
 
 public:
-
-    float x_min, x_max;
-    float y_min, y_max;
-
     CollisionArea() {
 
     }
@@ -34,6 +28,7 @@ public:
         this->y_min += offset.y;
         this->y_max += offset.y;
     }
+    
     // return (xmax1 >= xmin2) && (xmax2 >= xmin1);
     bool intersects(CollisionArea& testing_area) {
         return (
@@ -49,19 +44,11 @@ public:
         glm::vec2 colliding_center = { (colliding_area.x_min + colliding_area.x_max)/2, (colliding_area.y_min + colliding_area.y_max)/2 };
 
         if (isMovingTowards(colliding_center, this_center, desired_velocity)) {
-            // this's right side is collinging into left side
             glm::vec2 pos_difference = colliding_center - this_center;
-            // std::cout << pos_difference.x << " " << pos_difference.y << "\n";
 
-            if (abs(pos_difference.x) == abs(pos_difference.y)) {
-                std::cout << pos_difference.x << " A " << pos_difference.y << "\n";
-                return { 0.0f, 0.0f };
-            }
             if (abs(pos_difference.x) > abs(pos_difference.y)) {
-                std::cout << pos_difference.x << " B " << pos_difference.y << "\n";
                 return { 0.0f, desired_velocity.y };
             } else {
-                std::cout << pos_difference.x << " C " << pos_difference.y << "\n";
                 return { desired_velocity.x, 0.0f };
             }
         } else {
--- src/logic/movement_collision.h
+++ src/logic/movement_collision.h
@@ -13,50 +13,34 @@
 #include "../entity_management/collision_area.h"
 #include "../window_stuff/inputs.h"
 
-bool isOverlapping1D(float xmin1, float xmax1, float xmin2, float xmax2) {
-    return (xmax1 >= xmin2) && (xmax2 >= xmin1);
-}
-
 void processMovementCollisions(Timer timer, Inputs& inputs, std::shared_ptr<EntityManager> entity_manager, int player_id) {
     float delta = timer.getDeltaTime();
 
-    // for each collision area
+    // for each entity that has movement property
     for (auto& [a, a_movement] : entity_manager->movements) {
-        // if it is moving
+        // if a is moving
         if (a_movement.velocity != glm::vec2(0.0f, 0.0f)) {
-            glm::vec2 a_desired_velocity = entity_manager->getMovement(a).velocity;
-            // check for collisions
+            glm::vec2 a_desired_velocity = entity_manager->getMovement(a).velocity * delta;
+            // if a has collision
             auto a_collision_area_iterator = entity_manager->collision_areas.find(a);
             if (a_collision_area_iterator != entity_manager->collision_areas.end()) {
+                // for each other entity with collisions
                 for (auto& [b, b_collision_area] : entity_manager->collision_areas) {
-                    // glm::vec2 a_position = entity_manager->getRenderable(a).position;
-                    // glm::vec2 b_position = entity_manager->getRenderable(b).position;
-
                     CollisionArea a_new_collision_area = a_collision_area_iterator->second;
-                    a_new_collision_area.offsetBy(a_desired_velocity * delta);
+                    a_new_collision_area.offsetBy(a_desired_velocity);
 
+                    // check if a intersects with b
                     if (a_new_collision_area.intersects(b_collision_area) && a != b) {
-                        // std::cout << "intersects" << entity_manager->getRenderable(a).position.x << " " << entity_manager->getRenderable(a).position.y << "\n";
-                        // std::cout << "intersects" << entity_manager->getCollisionArea(a).x_min << " " << entity_manager->getCollisionArea(a).y_min << "\n";
+                        // resolve the desired velocity of a to one that is valid
                         a_desired_velocity = a_new_collision_area.resolveGoingInto(b_collision_area, a_desired_velocity);
-                        std::cout << "intersects" << a_desired_velocity.x << " " << a_desired_velocity.y << "\n";
-
-                        // entity_manager->getRenderable(a).position += (resolved_velocity * delta);
-                        // entity_manager->getMovement(a).velocity = resolved_velocity * delta;
-                        // entity_manager->getCollisionArea(a).offsetBy(resolved_velocity * delta);
                     }
                 }
-                entity_manager->getMovement(a).velocity = a_desired_velocity * delta;
-                entity_manager->getRenderable(a).position += (a_desired_velocity * delta);
-                entity_manager->getCollisionArea(a).offsetBy(a_desired_velocity * delta);
+                entity_manager->getCollisionArea(a).offsetBy(a_desired_velocity);
             }
-            entity_manager->getRenderable(a).position += entity_manager->getMovement(a).velocity * delta;
-            // entity_manager->getCollisionArea(player_id).offsetBy(entity_manager->getMovement(player_id).velocity);
-
+            entity_manager->getMovement(a).velocity = (a_desired_velocity);
+            entity_manager->getRenderable(a).position += (a_desired_velocity);
         }
-
     }
-
 }
 
 void updateDirections(std::shared_ptr<EntityManager> entity_manager) {
